fac3c12c642ede1d8bc6fe415654d0d3eaec6fef
Michael Geraghty <tup13771@temple.edu>
2024-11-14 15:21:58 -0500

    displays timer

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,15 +5,23 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var timerBinder: TimerService.TimerBinder? = null
-
+    lateinit var timerView : TextView;
+    val timerHandler = Handler(Looper.getMainLooper()){
+        timerView.text=it.what.toString()
+        true
+    }
     val serviceConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             timerBinder = service as TimerService.TimerBinder
+            timerBinder!!.setHandler(timerHandler)
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
@@ -24,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        timerView = findViewById<TextView>(R.id.textView)
         findViewById<Button>(R.id.startButton).setOnClickListener {
             bindService(
                 Intent(this, TimerService::class.java),
